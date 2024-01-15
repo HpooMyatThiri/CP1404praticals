a64e81011fdc72e5f21dca0171b777f325799317
hpoomyatthiri <hpoomyatthiri@my.jcu.edu.au>
2024-01-15 23:11:01 +0800

    Programs for pratical10

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,20 +1,27 @@
-name = input("Name: ")
-out_file = open('name.txt', 'w')
-print(name , file = out_file)
-out_file.close
+# name = input("Name: ")
+#
+# out_file = open('name.txt', 'w')
+# print(name , file = out_file)
+# out_file.write(name)
+# out_file.close
+#
+#
+# out_file = open('name.txt', 'r')
+# name = out_file.read()
+# print(f"Your name is {name}")
+# out_file.close
 
-out_file = open('name.txt', 'r')
-name = out_file.read()
-print(f"Your name is {name}")
-out_file.close
+with open('numbers.txt', 'r') as out_file:
+    content = out_file.read()
+    print(content)
 
+out_file = open('numbers.txt', 'r')
 total_number = 0
 for line in range(2):
     number = float(out_file.readline())
     total_number += number
 print(total_number)
 out_file.close
-
 
 out_file = open('numbers.txt', 'r')
 total_number = 0
